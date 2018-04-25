8d090202bf93b84078eb2cafce1252f69348ac89
Tanner Evins <tanner@tannerevins.com>
2018-04-24 21:48:12 -0500

    Restyle `Contact`

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,29 +1,26 @@
 import React from 'react'
-import { SocialIcon } from 'react-social-icons'
 import styles from './styles.css'
 import Page from '../Page'
 import Title from '../Title'
-
-const BOOKING_EMAIL = 'booking@drsoulsband.com'
-const COLOR = '#ddd'
+import Social from '../Social'
+import { BOOKING_EMAIL } from '../../constants'
 
 const Contact = () => (
   <Page id='contact' className={styles.contact}>
     <Title>Contact</Title>
     <div className={styles.content}>
-      <div>
-        Got a gig? Need the funk? We could be the band for you.
-        <br />
-        Email us at&nbsp;
-        <a href={`mailto:${BOOKING_EMAIL}`}>
-          {BOOKING_EMAIL}
-        </a>.
-      </div>
-      <div className={styles.social}>
-        <SocialIcon color={COLOR} url={`mailto:${BOOKING_EMAIL}`} />
-        <SocialIcon color={COLOR} url='https://www.facebook.com/DrSoulsBand/' />
-        <SocialIcon color={COLOR} url='https://www.instagram.com/drsoulsband/' />
-      </div>
+        <div>
+          <em>Got a gig? Need the funk?</em>
+        </div>
+        <div className={styles.tag}>
+          <strong>We could be the band for you</strong>
+        </div>
+        <div className={styles.emailUs}>
+          Book us at&nbsp;
+          <a href={`mailto:${BOOKING_EMAIL}`}>
+            {BOOKING_EMAIL}
+          </a>
+        </div>
     </div>
   </Page>
 )
